26d8e43af80a7d7c5fbcd715642ce026717f2cab
Weijie <52730904+simweijie@users.noreply.github.com>
2021-04-12 20:26:46 +0800

    Add files via upload

---
+++ code/index.py
@@ -0,0 +1,47 @@
+import sys
+import logging
+import pymysql
+import json
+import os
+
+#rds settings
+rds_endpoint = os.environ['rds_endpoint']
+username=os.environ['username']
+password=os.environ['password']
+db_name=os.environ['db_name']
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+#Connection
+try:
+    connection = pymysql.connect(host=rds_endpoint, user=username,
+        passwd=password, db=db_name)
+except pymysql.MySQLError as e:
+    logger.error("ERROR: Unexpected error: Could not connect to MySQL instance.")
+    logger.error(e)
+    sys.exit()
+logger.info("SUCCESS: Connection to RDS MySQL instance succeeded")
+
+def handler(event, context):
+    cur = connection.cursor()  
+## Retrieve Data    
+    query = "UPDATE Staff SET email='{}',name='{}',addr='{}',contactNo='{}',job='{}',status='{}',isAdmin='{}',branchId='{}' where id='{}'"\
+        .format(event['email'], event['name'], event['addr'],event['contactNo'],event['job'],event['status'],event['isAdmin'],event['branchId'],event['id'])
+    cur.execute(query)
+    connection.commit()
+    print(cur.rowcount, "record(s) affected")
+## Construct body of the response object
+    transactionResponse = {}
+# Construct http response object
+    responseObject = {}
+    responseObject['statusCode'] = 200
+    responseObject['headers'] = {}
+    responseObject['headers']['Content-Type']='application/json'
+    responseObject['headers']['Access-Control-Allow-Origin']='*'
+    responseObject['body'] = json.dumps(transactionResponse, sort_keys=True,default=str)
+    
+    #k = json.loads(responseObject['body'])
+    #print(k['uin'])
+
+    return responseObject
