799eb77dcc238e335dd3aedee6ee193880306ec4
Weijie <52730904+simweijie@users.noreply.github.com>
2021-04-16 01:17:25 +0800

    Add files via upload

--- code/index.py
+++ code/index.py
@@ -35,11 +35,11 @@ def handler(event, context):
     transactionResponse = {}
 # Construct http response object
     responseObject = {}
-    responseObject['statusCode'] = 200
-    responseObject['headers'] = {}
-    responseObject['headers']['Content-Type']='application/json'
-    responseObject['headers']['Access-Control-Allow-Origin']='*'
-    responseObject['body'] = json.dumps(transactionResponse, sort_keys=True,default=str)
+    # responseObject['statusCode'] = 200
+    # responseObject['headers'] = {}
+    # responseObject['headers']['Content-Type']='application/json'
+    # responseObject['headers']['Access-Control-Allow-Origin']='*'
+    responseObject['data'] = json.dumps(transactionResponse, sort_keys=True,default=str)
     
     #k = json.loads(responseObject['body'])
     #print(k['uin'])
